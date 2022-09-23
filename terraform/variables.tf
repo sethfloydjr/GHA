6effd24a5b0aa1de3518c787daf413ee96252d44
Seth Floyd <seth@sethfloydjr.com>
2022-09-23 16:08:58 -0400

    TF test

---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+########################## Authentication variables ###########################
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "AWS_ACCESS_KEY_ID" {}
+variable "AWS_SECRET_ACCESS_KEY" {}
+###############################################################################
